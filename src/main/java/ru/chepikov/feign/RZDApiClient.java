0e3711ec7104aab75ef476fba0d02ca12fc0255d
dima <chepikov.d.u@gmail.com>
2025-04-24 18:25:09 +0300

    Добавил время отправления и время прибытия поезда

--- src/main/java/ru/chepikov/feign/RZDApiClient.java
+++ src/main/java/ru/chepikov/feign/RZDApiClient.java
@@ -13,11 +13,6 @@ import java.util.Map;
 @FeignClient(name = "rzd", url = "https://ticket.rzd.ru/api/v1/railway-service/prices/train-pricing", configuration = FeignConfiguration.class)
 public interface RZDApiClient {
 
-    /*@GetMapping("?origin={origin}&destination={destination}&departureDate={departureDate}T00:00:00")*/
-    /*Map<String, Object> getPrices(@PathVariable Integer origin,*/
-    /*                              @PathVariable Integer destination,*/
-    /*                              @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate departureDate);*/
-
     @GetMapping
     String getPrices(@RequestParam Integer origin,
                                   @RequestParam Integer destination,
--- src/main/java/ru/chepikov/model/dto/train/Train.java
+++ src/main/java/ru/chepikov/model/dto/train/Train.java
@@ -5,9 +5,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 import ru.chepikov.model.dto.carrier.CarGroups;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Data
@@ -22,9 +23,19 @@ public class Train {
     @JsonProperty("CarGroups")
     private List<CarGroups> carGroupsList;
 
+    @JsonProperty("DepartureDateTime")
+    private LocalDateTime departureDateTime;
+
+    @JsonProperty("ArrivalDateTime")
+    private LocalDateTime arrivalDateTime;
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME;
+
     @Override
     public String toString() {
-        return "\nНомер поезда: " + trainNumber + "\n" + carGroupsList;
-
+        return "\nНомер поезда: " + trainNumber +
+                "\nОтправление поезда: " + departureDateTime.format(dateTimeFormatter) +
+                "\nПрибытие поезда: " + arrivalDateTime.format(dateTimeFormatter) + "\n" + carGroupsList +
+                "\n\n";
     }
 }
