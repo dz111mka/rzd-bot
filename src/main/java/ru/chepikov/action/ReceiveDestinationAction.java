98b421933d7ab1cb6f7a9055e68928378c62718e
dima <chepikov.d.u@gmail.com>
2026-05-10 23:36:41 +0300

    add state machine

---
+++ src/main/java/ru/chepikov/action/ReceiveDestinationAction.java
@@ -0,0 +1,19 @@
+package ru.chepikov.action;
+
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+import ru.chepikov.state_machine.BotEvents;
+import ru.chepikov.state_machine.BotStates;
+
+@Component("destinationAction")
+public class ReceiveDestinationAction implements Action<BotStates, BotEvents> {
+
+    @Override
+    public void execute(StateContext<BotStates, BotEvents> context) {
+        String station = context.getExtendedState().get("message", String.class);
+        
+        context.getExtendedState().getVariables().put("destinationStation", station);
+        context.getExtendedState().getVariables().put("result", "Подписка создана!");
+    }
+}
